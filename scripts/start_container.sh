45eb799d3681094252142ef1c628f257b5360a1e
Imash24 <heisash24@gmail.com>
2025-02-06 13:09:05 +0530

    buildcodedeploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+docker --version
+
+sudo systemctl enable --now docker
+
+docker pull heisash24/ashflask:latest
+
+docker run -p 5000:5000 heisash24/ashflask
---
+++ scripts/stop_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+container = sudo docker ps | awk '{print $1}'
+
+sudo docker rm $container
